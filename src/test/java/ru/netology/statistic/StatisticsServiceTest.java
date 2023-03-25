792f1b5ddc01e4e97121fdf00d765bfe7ef7f427
Jack <youngdragon0@gmail.com>
2023-03-25 19:27:35 +0300

    изменение теста для покрытия 100% кода

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,6 +1,6 @@
 package ru.netology.statistic;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
@@ -9,11 +9,11 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticService service = new StatisticService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {0, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
 
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 }
